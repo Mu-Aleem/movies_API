e7023bd748738338d9481540d3136e741eb66934
Aleem <se.aleemkhalid786@gmail.com>
2024-07-31 15:23:13 +0500

    set the error class

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+//routes import
+import userRouter from "./routes/users/user.routes.js";
+import moviesRouter from "./routes/movies/movies.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+//middlewares import
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 const app = express();
 
 app.use(
@@ -16,16 +24,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-//routes import
-import userRouter from "./routes/users/user.routes.js";
-import moviesRouter from "./routes/movies/movies.routes.js";
-
-import healthcheckRouter from "./routes/healthcheck.routes.js";
-
 //routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter);
 
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/movies", moviesRouter);
+
+app.use(errorHandler);
 
 export { app };
